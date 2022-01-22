11e34654463f93ace431191359d4ffb08d9d7904
nincentaur <nina.centauri@gmail.com>
2022-01-21 22:52:56 -0500

    Complete reverseString js exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function(str) {
+//using 3 methods:
+//String.prototype.split() -> splits string into an array of string by separating the string into sub strings
+//Array.prototype.reverse() which reverses array in place
+//Array.prototype.join() joins all elements of an array into a string
 
+const reverseString = function(str) {
+  return str.split('').reverse().join('');
 };
 
 // Do not edit below this line
