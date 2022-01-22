3c67963e29186d2c8a7561c62ce6107a6abf570f
nincentaur <nina.centauri@gmail.com>
2022-01-22 00:04:48 -0500

    Complete removeFromArray js exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+  const array = args [0];
+  const newArray = [];
+
+  array.forEach((item) => {
+    if (!args.includes(item)) {
+        newArray.push(item);
+    }
+  });
+
+  return newArray;
 
 };
 
